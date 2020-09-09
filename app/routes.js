c6ab2c78b6e1f6060781f912465dcf892ffae225
Rob Langdon <roblangdon@me.com>
2020-09-09 16:11:46 +0100

    added v1-3

--- app/routes.js
+++ app/routes.js
@@ -7,8 +7,8 @@ const radioButtonRedirect = require('radio-button-redirect')
 router.use(radioButtonRedirect)
 
 router.use('/', (req, res, next) => {
-  req.session.data.version = req.session.data.version  || "1-2"
-  req.session.data.version_list= req.session.data.version_list || ["1-0","1-1","1-2"]
+  req.session.data.version = req.session.data.version  || "1-3"
+  req.session.data.version_list= req.session.data.version_list || ["1-0","1-1","1-2","1-3"]
   res.locals.currentURL = req.originalUrl; //current screen
   req.session.data.currentURL = req.path
   res.locals.prevURL = req.get('Referrer'); // previous screen
