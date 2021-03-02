0ddd8f3a8bb57eb9c6687192ff129fc38fda2584
Rob Langdon <rob.langdon@defra.gov.uk>
2021-03-02 18:54:14 +0000

    Updated to v1-15, added PEURD-39 and PEURD-221
    Former-commit-id: bd231621cc10fa635cc401a4fec14523b197ce9b

--- app/routes/dev.js
+++ app/routes/dev.js
@@ -7,7 +7,7 @@ module.exports = function(router) {
   var client3 = new RestClient()
 
   // ADD extra routing here if needed.
-  require('./dev_v14.js')(router)
+  require('./dev_v15.js')(router)
 
 
 
