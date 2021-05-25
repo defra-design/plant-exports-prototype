a42e3a501cfe6a0eabb7245e4fe6f7e38dda1be2
Rob Langdon <rob.langdon@defra.gov.uk>
2021-05-25 21:49:15 +0100

    PEURD-244 and PEURD-236 - iterations on GSI and HMI respectively
    Former-commit-id: b1028060b06571a328dcd51c09fd83011ffecff2

--- app/routes/dev.js
+++ app/routes/dev.js
@@ -7,7 +7,7 @@ module.exports = function(router) {
   var client3 = new RestClient()
 
   // ADD extra routing here if needed.
-  require('./dev_v20.js')(router)
+  require('./dev_v21.js')(router)
 
 
 
