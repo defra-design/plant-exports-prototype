e7820048cc50159b10ad6636528980d4b8aa0a6c
Martin Coulthurst <mc@lightness.co.uk>
2021-07-08 09:59:17 +0100

    Update version links

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,7 @@ router.use(radioButtonRedirect)
 
 router.use('/', (req, res, next) => {
   req.session.data.version = req.session.data.version  || "1-23"
-  req.session.data.version_list= req.session.data.version_list || ["1-23","1-22","1-21","1-20","1-19","1-18","1-17","1-16","1-15","1-14","1-13","1-12","1-11","1-10","1-9","1-8","1-7","1-6","1-5","1-4","1-3","1-2","1-1","1-0"]
+  req.session.data.version_list= req.session.data.version_list || ["1-24","1-23","1-22","1-21","1-20","1-19","1-18","1-17","1-16","1-15","1-14","1-13","1-12","1-11","1-10","1-9","1-8","1-7","1-6","1-5","1-4","1-3","1-2","1-1","1-0"]
   res.locals.currentURL = req.originalUrl; //current screen
   req.session.data.currentURL = req.path
   res.locals.prevURL = req.get('Referrer'); // previous screen
