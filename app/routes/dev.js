5a72b00abac829f0d0a3bf283e9b5904053b5461
Chris Harding <chris@transparentux.co.uk>
2023-05-19 12:41:59 +0100

    Created a new version of the prototype (v2.2) for adding all new designs and content to in Sprint 73.

--- app/routes/dev.js
+++ app/routes/dev.js
@@ -7,6 +7,7 @@ module.exports = function(router) {
   var client3 = new RestClient()
 
   // ADD extra routing here if needed.
+  require('./dev_v2-2.js')(router)
   require('./dev_v2-1.js')(router)
   require('./dev_v2-0.js')(router)
   require('./dev_v42.js')(router)
