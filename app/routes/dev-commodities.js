b3d313498d8199eaadd03121ff6a538e43a78ac9
Chris Harding <chris@transparentux.co.uk>
2023-11-08 17:51:21 +0000

    Created a new major version of the prototype (v3.0) to merge all ePhyto designs into.

--- app/routes/dev-commodities.js
+++ app/routes/dev-commodities.js
@@ -7,6 +7,7 @@ module.exports = function(router) {
   var client3 = new RestClient()
 
   // ADD extra routing here if needed.
+  require('./dev_v3-0.js')(router)
   require('./dev_v2-6.js')(router)
   require('./dev_v2-5.js')(router)
   require('./dev_v2-4.js')(router)
